b83bda194adf392986d15ec1315fb3560c63a11d
arrahimi <arrahimi@avans.nl>
2017-09-30 20:32:07 +0200

    added clion files

---
+++ cmake-build-debug/CMakeFiles/GroepH.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GroepH.dir/src/engine/source/assetsmanager.cpp.o"
+  "CMakeFiles/GroepH.dir/src/engine/source/Engine.cpp.o"
+  "CMakeFiles/GroepH.dir/src/game/main.cpp.o"
+  "GroepH.pdb"
+  "GroepH"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GroepH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
