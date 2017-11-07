5f60418a0ff4f402af73d2f60234c8194367e955
Niels Kop <nkop1@avans.nl>
2017-11-07 14:47:29 +0100

    zou moeten werken maar nog geen gitignore

---
+++ Shooter/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../Engine/Engine.h"
+
+int main(int argc, char *argv[]){
+    std::cout << "Hello, World!" << std::endl;
+
+	auto engine = new Engine::MainManager();
+	//werkt nog niet vanwege link problemen
+	//AudioManager::instance()->loadBGM("pokemon");
+	//AudioManager::instance()->playBGM();
+	std::cout << engine->Message;
+
+
+	std::cin.get();
+
+    return 0;
+}
