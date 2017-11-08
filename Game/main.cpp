c98c5e1c462a30e56899b8329575d379d6310cfc
Nkmol <sandermol95@hotmail.com>
2017-11-08 18:46:30 +0100

    Add Game project

---
+++ Game/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include "Engine.h"
+//#include "AudioManager.h"
+
+int main(int argc, char *argv[]) {
+	std::cout << "Hello, World!" << std::endl;
+
+	//auto engine = new Engine::MainManager();
+	//werkt nog niet vanwege link problemen
+	//AudioManager::instance()->loadBGM("pokemon");
+	//AudioManager::instance()->playBGM();
+	//std::cout << engine->Message;
+
+
+	std::cin.get();
+
+	return 0;
+}
