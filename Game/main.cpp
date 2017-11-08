dcc5d935f45b319728aaa109e72661d69f3a41c8
Nkmol <sandermol95@hotmail.com>
2017-11-08 20:15:12 +0100

    Link engine and game

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-//#include "Engine.h"
+#include "Engine.h"
 //#include "AudioManager.h"
 
 int main(int argc, char *argv[]) {
 	std::cout << "Hello, World!" << std::endl;
 
-	//auto engine = new Engine::MainManager();
+	auto engine = new Engine::MainManager();
 	//werkt nog niet vanwege link problemen
 	//AudioManager::instance()->loadBGM("pokemon");
 	//AudioManager::instance()->playBGM();
-	//std::cout << engine->Message;
+	std::cout << engine->Message;
 
 
 	std::cin.get();
