136d6756d70e0212757cf0816f8aed7921d587f6
nkop <nkop1@avans.nl>
2017-11-27 19:32:05 +0100

    Level Object (#31)
    * Level Object
    
    * movement fixed

---
+++ Game/Level.cpp
@@ -0,0 +1,60 @@
+#include "Level.h"
+
+Level::Level(const int level) : _level(level)
+{
+	Init();
+}
+
+void Level::Init()
+{
+	MapManager::Instance().Init("../content/map/halflife.tmx");
+
+
+	auto player = make_shared<Player>("soldier", 100, 300);
+	auto uzi = make_unique<Weapon>(Uzi());
+	player->addWeapon(*uzi);
+
+	_objs.emplace_back(player);
+
+	// save pointer seperate
+	_player = player;
+	_flockController.generateFlock(20, 100, 900, _player, 15.0f);
+}
+
+void Level::HandleEvents(SDL_Event event)
+{	
+	if (InputManager::instance().isMouseMoved(event)) {
+		// RECALCULATE players angle to mouse ONLY IF the mouse has been moved.
+		int angle = InputManager::instance().recalculateMouseAngle(*_player);
+
+		// setAngle is called, so that the player aims towards the mouse, even when the player is not moving.
+		_player->setAngle(angle);
+	}
+
+	if (InputManager::instance().isMouseClicked(event)) {
+		_player->shoot();
+	}
+
+	Point direction = InputManager::instance().getDirection(event);
+
+	int angle = InputManager::instance().calculateMouseAngle(*_player);
+
+	_player->setAngle(angle);
+	_player->Move(direction);
+}
+
+void Level::Update(float time)
+{
+	for (auto &&obj : _objs) {
+		obj->update(time);
+	}
+	_flockController.updateFlocks(time);
+}
+
+void Level::Draw()
+{
+	for (auto &&obj : _objs) {
+		obj->draw();
+	}
+	_flockController.drawFlocks();
+}
--- Game/PlayingState.cpp
+++ Game/PlayingState.cpp
@@ -21,63 +21,25 @@ void PlayingState::HandleEvents(Game &game) {
 		if (inputManager.isPauseResume(event)) {
 			auto state = make_unique<PausedState>();
 			game.ChangeState(std::move(state));
-		}
-
-        if (inputManager.isMouseMoved(event)) {
-            // RECALCULATE players angle to mouse ONLY IF the mouse has been moved.
-            int angle = inputManager.recalculateMouseAngle(*_player);
-
-            // setAngle is called, so that the player aims towards the mouse, even when the player is not moving.
-            _player->setAngle(angle);
-        }
-
-        if (inputManager.isKeyDown(event)) {
-            Point direction = inputManager.getDirection(event);
-
-            int angle = inputManager.calculateMouseAngle(*_player);
-
-            _player->setAngle(angle);
-            _player->Move(direction);
-        }
-
-        if (inputManager.isKeyUp(event)) {
-            _player->stopMove();
-        }
-
-        if (inputManager.isMouseClicked(event)) {
-            _player->shoot();
-        }
-
+		}        
         if (inputManager.isQuit(event)) {
             game.Quit();
         }
+		game.GetLevel()->HandleEvents(event);
     }
 }
 
 void PlayingState::Update(Game& game, float time)
 {
-	// The reference & to the std::unique_ptr avoids the copying and you can use the uniqe_ptr without dereferencing.
-	for (auto &&obj : _objs) {
-		obj->update(time);
-	}
-	flockController.updateFlocks(time);
+	game.GetLevel()->Update(time);
+
 }
 
 void PlayingState::Draw(Game &game) {
-    for (auto &&obj : _objs) {
-        obj->draw();
-    }
-    flockController.drawFlocks();
+	game.GetLevel()->Draw();
+
 }
 
 void PlayingState::Init() {
-	auto player = make_shared<Player>("soldier", 100, 300);
-	auto uzi = make_unique<Weapon>(Uzi());
-    player->addWeapon(*uzi);
-
-    _objs.emplace_back(player);
-
-    // save pointer seperate
-    _player = player;
-    flockController.generateFlock(20, 100, 900, _player, 15.0f);
+	
 }
