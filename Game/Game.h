e6bdc042c277e98c09c61d8e4b80a5c2030dc99a
nkop <nkop1@avans.nl>
2017-11-27 15:26:16 +0100

    no more memory leaks

--- Game/Game.h
+++ Game/Game.h
@@ -14,15 +14,15 @@ public:
 	void Init() const;
 	void ChangeState(std::unique_ptr<State>&& state);
 	void PopState();
-	void Quit() const;
-	void Run(unsigned fps);
+	void Quit();
+	void Run(const unsigned fps);
 	void HandleEvents();
 	void Update(float time);
 	void Draw();
 private:
 	// the stack of states
 	std::vector<std::unique_ptr<State>> _states;
-
+	bool isRunning;
 	Engine _mainManager;
 };
 
