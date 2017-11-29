34483fa9009d86dd67782910fc5cf3bb335fe16f
Mevlüt Özdemir <m1ozdemir@hotmail.com>
2017-11-29 21:49:17 +0100

    remove cmakefiles

--- CMakeFiles/content/map/tilesetje.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetje" tilewidth="16" tileheight="16" margin="1" tilecount="1131" columns="39" backgroundcolor="#000000">
- <grid orientation="orthogonal" width="32" height="32"/>
- <image source="roguelikeCity_magenta.png" width="628" height="475"/>
-</tileset>
