f04fabd8fe62b911345618ff81c588a2f2d62c75
arrahimi <arrahimi@avans.nl>
2017-11-30 11:30:01 +0100

    temp fix for fps added

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -44,7 +44,7 @@ void Game::Run(const unsigned int targetFps)
 	// Reference explenation: http://gamesfromwithin.com/casey-and-the-clearly-deterministic-contraptions
 	// Reference code example: https://gist.github.com/mariobadr/673bbd5545242fcf9482
 
-	TickerTime timer{targetFps, 120};
+	TickerTime timer{targetFps, 320};
 
 	#pragma region init timer
 	timer.OnCatchUp([&]()
