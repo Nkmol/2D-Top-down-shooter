6974333c208ad5147565914eb7376c374fc5ca57
Niels Kop <nielio_kop@hotmail.com>
2017-12-04 17:24:17 +0100

    wave json

---
+++ Game/Wave.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+struct FlockVars
+{
+	std::string type;
+	int amount;
+	int minPos;
+	int maxPos;
+};
+class Wave
+{
+	int _id;
+	float _time;
+	float _multiplier;
+	std::vector<FlockVars> _flocksVars;
+
+public:
+	const int get_id() const { return _id; }
+	void set_id(const int id) { _id = id; }
+	const float get_time() const { return _time; }
+	void set_time(const float time) { _time = time; }
+	const float get_multiplier() const { return _multiplier; }
+	void set_multiplier(const float multiplier) { _multiplier = multiplier; }
+	void set_flocksvars(const std::vector<FlockVars> vars) { _flocksVars = vars; }
+};
+
+void from_json(const json& j, Wave value) 
+{
+
+}
