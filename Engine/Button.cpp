7196a08d5a150b8fa71ad5e34338c42799dc20f1
Jeroentje61 <jfpbreke@avans.nl>
2017-12-08 12:52:34 +0100

    Changes to Button

--- Engine/Button.cpp
+++ Engine/Button.cpp
@@ -1,10 +1,10 @@
-#include "RenderManager.h"
-#include "AssetManager.h"
 #include "Button.h"
+
 Button::Button(std::string &mediatoken, int x1, int y1, int width, int height) : GameObject(Point(x1, y1), width, height) {
-	x1Pos = x1;
-	y1Pos = y1;
+	_x1Pos = x1;
+	_y1Pos = y1;
 	setX2andY2(x1, y1, width, height);
+
 	SDL_Surface* surface = AssetManager::Instance().LoadSurface(mediatoken);
 	if (!surface)
 		cout << SDL_GetError() << endl;
@@ -17,24 +17,38 @@ Button::Button(std::string &mediatoken, int x1, int y1, int width, int height) :
 	}
 }
 
-Button::Button() {}
+Button::Button() 
+{
+
+}
 
 void Button::setX2andY2(int x1, int y1, int width, int height)
 {
-	x2Pos = x1 + width;
-	y2Pos = y1 + height;
+	_x2Pos = x1 + width;
+	_y2Pos = y1 + height;
 }
 
 void Button::drawButton() {
 	int w, h;
 	SDL_QueryTexture(this->_sprite, nullptr, nullptr, &w, &h);
 
-	SDL_Rect destinationRectangle = { static_cast<int>(x1Pos), static_cast<int>(y1Pos), w, h };
+	SDL_Rect destinationRectangle = { static_cast<int>(_x1Pos), static_cast<int>(_y1Pos), w, h };
 	RenderManager::Instance().DrawTexture(this->_sprite, nullptr, &destinationRectangle, 0);
 }
 
-int Button::getX1() { return this->x1Pos; }
-int Button::getY1() { return this->y1Pos; }
-int Button::getY2() { return this->y2Pos; }
-int Button::getX2() { return this->x2Pos; }
+int Button::getX1() {
+	return this->_x1Pos; 
+}
+
+int Button::getY1() { 
+	return this->_y1Pos; 
+}
+
+int Button::getY2() { 
+	return this->_y2Pos; 
+}
+
+int Button::getX2() { 
+	return this->_x2Pos; 
+}
 
--- Engine/Button.h
+++ Engine/Button.h
@@ -1,23 +1,15 @@
 #pragma once
 #include <string>
-#include "sdl.h"
+#include "SDL.h"
 #include "GameObject.h"
 class Button : public GameObject {
 public:
 	Button(std::string &mediatoken, int x1, int y1, int width, int height);
 	Button();
 	void drawButton();
-	int getX1();
-	int getY1();
-	int getY2();
-	int getX2();
+	int getX1(), getY1(), getY2(), getX2();
 private:
-	int x1Pos;
-	int x2Pos;
-	int y1Pos;
-	int y2Pos;
-	int width;
-	int height;
+	int _x1Pos, _x2Pos, _y1Pos, _y2Pos;
 	SDL_Texture* _sprite;
 	void setX2andY2(int, int, int, int);
 };
