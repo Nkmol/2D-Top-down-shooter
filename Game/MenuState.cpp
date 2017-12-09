78d40319b5413b1171adb2206eae467aa1e564be
Niels Kop <nielio_kop@hotmail.com>
2017-12-09 15:05:36 +0100

    revert menu substate Add filepicker

--- Game/MenuState.cpp
+++ Game/MenuState.cpp
@@ -3,11 +3,16 @@
 #include "PlayingState.h"
 #include "AudioManager.h"
 #include "Game.h"
-#include "MenuMainState.h"
+#include <experimental/filesystem>
+#include <nfd.h>
+#include <memory>
+
+namespace fs = std::experimental::filesystem;
 
 MenuState::MenuState()
-{ 
+{
 }
+
 MenuState::~MenuState()
 {
 
@@ -22,58 +27,92 @@ void MenuState::HandleEvents(Game& game)
 			game.Quit();
 		else if (inputManager.IsMouseClicked(ev))
 		{
-			if (ev.GetEventValue().button.x > _quitButton.getX1() && ev.GetEventValue().button.x < _quitButton.getX2() && ev.GetEventValue().button.y > _quitButton.getY1() && ev.GetEventValue().button.y < _quitButton.getY2()) {
+			if (ev.GetEventValue().button.x > _newgameButton.getX1() && ev.GetEventValue().button.x < _newgameButton.getX2() && ev.GetEventValue().button.y > _newgameButton.getY1() && ev.GetEventValue().button.y < _newgameButton.getY2()) {
+				//New game
+				auto state = std::make_unique<PlayingState>();
+				game.ChangeState(std::move(state));
+				AudioManager::Instance().StopBGM();
+				game.SetLevel(1);
+				// todo, alle draw functies naar rendermanager: functie: void DrawObject(MoveableObject object);
+			}
+
+			else if (ev.GetEventValue().button.x > _loadgameButton.getX1() && ev.GetEventValue().button.x < _loadgameButton.getX2() && ev.GetEventValue().button.y > _loadgameButton.getY1() && ev.GetEventValue().button.y < _loadgameButton.getY2()) {
+				std::string savedGame;
+
+				fs::path path{ fs::current_path().parent_path() };
+
+				path += "\\content\\saves";
+				std::wstring ws(path);
+				std::string str(ws.begin(), ws.end());
+
+				nfdchar_t *outPath = NULL;
+				nfdresult_t result = NFD_OpenDialog("json", str.c_str(), &outPath);
+				if (result == NFD_OKAY)
+				{
+					savedGame = outPath;
+					free(outPath);
+
+				}
+				else if (result == NFD_CANCEL)
+				{
+					puts("User pressed cancel.");
+				}
+				else
+				{
+					printf("Error: %s\n", NFD_GetError());
+				}
+			}
+
+			else if (ev.GetEventValue().button.x > _creditsButton.getX1() && ev.GetEventValue().button.x < _creditsButton.getX2() && ev.GetEventValue().button.y > _creditsButton.getY1() && ev.GetEventValue().button.y < _creditsButton.getY2()) {
+				//Credits
+			}
+			else if (ev.GetEventValue().button.x > _quitButton.getX1() && ev.GetEventValue().button.x < _quitButton.getX2() && ev.GetEventValue().button.y > _quitButton.getY1() && ev.GetEventValue().button.y < _quitButton.getY2()) {
 				game.Quit();
 			}
 			else if (ev.GetEventValue().button.x > _muteButton.getX1() && ev.GetEventValue().button.x < _muteButton.getX2() && ev.GetEventValue().button.y > _muteButton.getY1() && ev.GetEventValue().button.y < _muteButton.getY2())
 			{
 				//Mute
-				if (muted == 1) {
+				if (_muted == 1) {
 					AudioManager::Instance().StopBGM();
 					RenderManager::Instance().Clear();
-					muted = 0;
+					_muted = 0;
 				}
 				else {
 					AudioManager::Instance().InitMusicPlayer();
 					AudioManager::Instance().LoadBGM("mainmenu");
 					AudioManager::Instance().PlayBGM();
-					muted = 1;
+					_muted = 1;
 				}
 			}
 		}
-		_subStates.back()->HandleEvents(game, *this, ev);
 	}
 }
 
 void MenuState::Update(Game& game, float time)
 {
-	_subStates.back()->Update(game, time);
+
 }
 
 void MenuState::Draw(Game& game)
 {
 	RenderManager::Instance().DrawTexture(_background->GetTexture(), NULL, NULL);
 
+	_newgameButton.drawButton();
+	_loadgameButton.drawButton();
+	_creditsButton.drawButton();
 	_muteButton.drawButton();
 	_quitButton.drawButton();
-
-	_subStates.back()->Draw();
 }
 
 void MenuState::Init()
-{	
-	_subStates.push_back(make_unique<MenuMainState>());
-	_muteButton = Button(_muteString, config::width - 100, config::height - 100, 75, 75);
-	_quitButton = Button(_quitString, (config::width / 2) - 123, (config::height / 3) * 2, 225, 45);
+{
 	_background = std::make_unique<Texture>(AssetManager::Instance().LoadTexture("menu-wallpaper"));
 
+	_newgameButton = Button(_newgameString, (config::width / 2) - 123, (config::height / 3) * 0.5, 225, 45);
+	_loadgameButton = Button(_loadgameString, (config::width / 2) - 123, (config::height / 3) * 1, 225, 45);
+	_creditsButton = Button(_creditsString, (config::width / 2) - 123, (config::height / 3) * 1.5, 225, 45);
+	_muteButton = Button(_muteString, (config::width / 2) + 200, (config::height / 3) * 2, 75, 75);
+	_quitButton = Button(_quitString, (config::width / 2) - 123, (config::height / 3) * 2, 225, 45);
+
 	int muted = 0;
-
-	_subStates.back()->Init();
-}
-
-void MenuState::ChangeState(std::unique_ptr<MenuSubState> substate)
-{
-	_subStates.push_back(std::move(substate));
-	_subStates.back()->Init();
 }
--- Game/MenuState.h
+++ Game/MenuState.h
@@ -1,14 +1,9 @@
 #pragma once
 #include <string>
-#include <memory>
 #include "State.h"
 #include "Config.h"
 #include "Button.h"
 #include "Texture.h"
-#include <json.hpp>
-
-class MenuSubState;
-class Game;
 
 class MenuState : public State
 {
@@ -20,15 +15,17 @@ public:
 	void Update(Game& game, float time) override;
 	void Draw(Game& game) override;
 	void Init() override;
-	void ChangeState(std::unique_ptr<MenuSubState> substate);
 private:
-
+	std::string _newgameString = "newgamebutton";
+	std::string _loadgameString = "loadgamebutton";
+	std::string _creditsString = "creditsbutton";
 	std::string _muteString = "mutebutton";
 	std::string _quitString = "quitbutton";
-	int muted;
-
+	int _muted;
+	Button _newgameButton;
+	Button _loadgameButton;
+	Button _creditsButton;
 	Button _muteButton;
 	Button _quitButton;
 	std::unique_ptr<Texture> _background;
-	std::vector<std::unique_ptr<MenuSubState>> _subStates;
 };
