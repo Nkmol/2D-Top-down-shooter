58333399780b9c202043cf8cc2b3e853a3a010b2
Mevlüt Özdemir <m1ozdemir@hotmail.com>
2017-12-13 15:39:23 +0100

    anim

---
+++ Engine/AnimationManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mevlüt Özdemir on 13-12-17.
+//
+
+#include "AnimationManager.h"
---
+++ Engine/AnimationManager.h
@@ -0,0 +1,14 @@
+//
+// Created by Mevlüt Özdemir on 13-12-17.
+//
+
+#ifndef SHOOTER_ANIMATIONMANAGER_H
+#define SHOOTER_ANIMATIONMANAGER_H
+
+
+class AnimationManager {
+
+};
+
+
+#endif //SHOOTER_ANIMATIONMANAGER_H
