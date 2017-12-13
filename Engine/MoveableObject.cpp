2969c6e4f7a023b2caac491469dd78c6f6541c2c
Mevlüt Özdemir <m1ozdemir@hotmail.com>
2017-12-13 15:33:41 +0100

    merge

--- Engine/MoveableObject.cpp
+++ Engine/MoveableObject.cpp
@@ -64,3 +64,62 @@ void MoveableObject::onBaseCollision(GameObject object)
 	//hide();
 }
 
+
+const string &MoveableObject::GetState() const {
+    return _state;
+}
+
+void MoveableObject::SetState(const string &_state) {
+    this->_state = _state;
+    setCurrentSpriteIndex(-1);
+}
+
+
+void MoveableObject::SetFrames(int _frames) {
+    MoveableObject::_frames = _frames;
+}
+
+void MoveableObject::HandleAnimationFinished() {
+    setCurrentSpriteIndex(-1);
+}
+
+float MoveableObject::getAnimationTimer() const {
+    return animationTimer;
+}
+
+void MoveableObject::SetAnimationTimer(float animationTimer) {
+    MoveableObject::animationTimer = animationTimer;
+}
+
+void MoveableObject::DecreaseAnimationTimer(float by) {
+    MoveableObject::animationTimer -= by;
+}
+
+bool MoveableObject::IsReadyForAnimation() const {
+    return getAnimationTimer() <= 0;
+}
+
+void MoveableObject::ChangeSprite(const std::string &spriteToken) {
+    _sprite = AssetManager::Instance().loadTexture(spriteToken);
+
+    SDL_QueryTexture(this->_sprite, nullptr, nullptr, &this->width, &this->height);
+    this->spriteToken = spriteToken;
+
+}
+
+void MoveableObject::setCurrentSpriteIndex(int index) {
+    MoveableObject::currentSprite = index;
+}
+
+
+int MoveableObject::GetNextSprite() {
+    return ++currentSprite;
+}
+
+string MoveableObject::getAnimationToken() {
+    return this->token;
+}
+
+bool MoveableObject::AnimationFinished() {
+    return currentSprite >= _frames;
+}
