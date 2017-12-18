e29b3cf6639feecaa809b07e281da863b588cac0
Nkmol <sandermol95@hotmail.com>
2017-12-18 18:59:26 +0100

    Refactor button

--- Engine/headers/Button.h
+++ Engine/headers/Button.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <string>
 #include "GameObject.h"
+#include "Event.h"
+#include <functional>
 
 
 class Button : public GameObject {
+	typedef function<void()> DelegateFunction;
 public:
-	Button(const std::string mediatoken, int x1, int y1, int width, int height);
+	// OLD
+	Button(const std::string& mediatoken, int x1, int y1, int width, int height);
+	// NEW
+	Button(const std::string& mediatoken, const Point& startPosition, const Point& size);
+	Button(const std::string& mediatoken, const Point& startPosition, const Point& size, const DelegateFunction&& del);
 	Button();
-	void drawButton();
 	int getX1();
 	int getY1();
 	int getY2();
 	int getX2();
+	bool IsClicked(Event ev) const;
+	void Click() const;
 private:
+	DelegateFunction _clickFuncton;
+
 	int x1Pos;
 	int x2Pos;
 	int y1Pos;
