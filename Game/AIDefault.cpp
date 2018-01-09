517b7b4b269680ecb364c704fec52d03b853ebc3
jarricvankrieken <jkrieken@avans.nl>
2018-01-09 13:32:12 +0100

    jiku

--- Game/AIDefault.cpp
+++ Game/AIDefault.cpp
@@ -1,4 +1,3 @@
-
 #include "AIDefault.h"
 #include "EnemyBase.h"
 
@@ -8,9 +7,7 @@ void AIDefault::Update(float time)
 	Cohese();
 	Seperate();
 	CohesePlayer();
-
 	
-
 	auto& coordinates = _owner->GetCoordinates();
 	auto& destinationPoint = _owner->GetDestinationPoint();
 
@@ -25,34 +22,6 @@ void AIDefault::Update(float time)
 void AIDefault::Align()
 {
 	_owner->SetDestinationPoint(_owner->_player->GetCoordinates());
-
-
-	//int count = 0;
-	//Vector2f v(0.0f, 0.0f);
-
-	//for (auto& a : *_konijnArray)
-	//{
-	//	if (a.get() == this)
-	//		continue;
-
-	//	float dis_x = ((a->Position->x + 5 / 2.0f) - (Position->x + 5 / 2.0f));
-	//	float dis_y = ((a->Position->y + 5 / 2.0f) - (Position->y + 5 / 2.0f));
-	//	float distance = abs(sqrtf(dis_x * dis_x + dis_y * dis_y));
-
-	//	if (distance > 0 && distance <= RADIUS)
-	//	{
-	//		v.Add(*a->Velocity);
-	//		count++;
-	//	}
-	//}
-
-	//if (count > 0)
-	//	v.Divide((float)count);
-
-	//v.Limit(MAX_FORCE);
-
-	//return v;
-
 }
 
 void AIDefault::Cohese()
@@ -74,56 +43,6 @@ void AIDefault::Cohese()
 	const auto forceDirection = Helper::calculateAngle(coordinates.x, coordinates.y, massCenter.x, massCenter.y);
 
 	_owner->ApplyForce(0.1, forceDirection);
-
-	//Vector2f sum(0.0f, 0.0f);
-	//int count = 0;
-
-	//for (auto& a : *_konijnArray)
-	//{
-	//	if (a.get() == this)
-	//		continue;
-
-	//	float dis_x = ((a->Position->x + 5 / 2.0f) - (Position->x + 5 / 2.0f));
-	//	float dis_y = ((a->Position->y + 5 / 2.0f) - (Position->y + 5 / 2.0f));
-	//	float distance = abs(sqrtf(dis_x * dis_x + dis_y * dis_y));
-
-	//	if (distance > 0 && distance <= NEIGHBOUR_RADIUS)
-	//	{
-	//		sum.Add(*a->Position);
-	//		count++;
-	//	}
-	//}
-
-	//if (count > 0)
-	//{
-	//	Vector2f steer(0.0f, 0.0f);
-	//	sum.Divide((float)count);
-
-	//	// A vector pointing from the location to the target
-	//	Vector2f desired = (sum - *Position);
-
-	//	// Distance from the target is the magnitude of the vector
-	//	float dis = desired.Magnitude();
-
-	//	// If the distance is greater than 0, calc steering
-	//	// (otherwise return zero vector)
-	//	if (dis > 0)
-	//	{
-	//		desired.Normalize();
-
-	//		// Two options for desired vector magnitude
-	//		// (1 -- based on distance, 2 -- maxspeed)
-	//		if (dis < NEIGHBOUR_RADIUS * 0.5f)
-	//			desired.Multiply(MAX_SPEED * (dis / NEIGHBOUR_RADIUS)); // This damping is arbitrary
-	//		else
-	//			desired.Multiply(MAX_SPEED);
-
-	//	}
-	//	return desired.Limit(MAX_FORCE);
-	//}
-	//return sum; // No neighbours, no cohesion
-
-
 }
 
 void AIDefault::Seperate()
@@ -147,10 +66,12 @@ void AIDefault::Seperate()
 			}
 	}
 
-	for (const auto& other : *PhysicsManager::Instance().getCollidables()) {
-		const auto& oX = other.getMidX();
-		const auto& oY = other.getMidY();
-		const auto& oWeight = other.getWidth() * other.getHeight() * COLLIDABLEWEIGHTMULTIPLIER;
+	std::vector<GameObject*> temp = {};
+	MapManager::Instance().getNearbyCollidables(coordinates, &temp);
+	for (const auto& other : temp) {
+		const auto& oX = other->getMidX();
+		const auto& oY = other->getMidY();
+		const auto& oWeight = other->getWidth() * other->getHeight() * COLLIDABLEWEIGHTMULTIPLIER;
 		const auto& m = coordinates;
 		const Point squared((oX - m.x) * (oX - m.x), (oY - m.y) * (oY - m.y));
 		const auto squareDist = squared.x + squared.y;
@@ -162,61 +83,10 @@ void AIDefault::Seperate()
 			_owner->SetDestinationPoint(_owner->GetDestinationPoint() + scaledVector);
 		}
 	}
-
-	//Vector2f v(0.0f, 0.0f);
-	//Vector2f temp(0.0f, 0.0f);
-	//int count = 0;
-
-	//// Surrounding bees
-	//for (auto& a : *_konijnArray)
-	//{
-	//	if (a.get() == this)
-	//		continue;
-
-	//	float dis_x = ((a->Position->x + 5 / 2.0f) - (Position->x + 5 / 2.0f));
-	//	float dis_y = ((a->Position->y + 5 / 2.0f) - (Position->y + 5 / 2.0f));
-	//	float distance = abs(sqrtf(dis_x * dis_x + dis_y * dis_y));
-
-	//	if (distance > 0 && distance <= SEPERATION_RADIUS)
-	//	{
-	//		// Normalized, weighted by distance vector pointing away from the neighbour
-	//		temp.Set(*Position).Substract(*a->Position).Normalize();
-	//		temp.Multiply(1.2f);
-	//		temp.Divide(distance).Limit(MAX_FORCE);
-	//		v.Add(temp);
-	//		count++;
-	//	}
-	//}
-
-	//if (count > 0)
-	//	v.Divide((float)count);
-
-	//return v;
 }
 
 void AIDefault::CohesePlayer()
 {
-	//Vector2f v(0.0f, 0.0f);
-	//Vector2f temp(0.0f, 0.0f);
-	//int count = 0;
-
-	//// Beekeeper
-	//float dis_x = (_schaap->GetTruePositionX() - (Position->x + width / 2.0f));
-	//float dis_y = (_schaap->GetTruePositionY() - (Position->y + height / 2.0f));
-	//float distance = abs(sqrtf(dis_x * dis_x + dis_y * dis_y));
-
-	//if (distance > 0 && distance <= 50)
-	//{
-	//	temp.Set(*Position).Substract(*_schaap->GetPositionX(), *_schaap->GetPositionY()).Normalize();
-	//	temp.Divide(-distance).Limit(MAX_FORCE);
-	//	v.Add(temp);
-	//	count++;
-	//}
-
-	//if (count > 0)
-	//	v.Divide((float)count);
-
-	//return v;
 }
 
 void AIDefault::GoTarget()
