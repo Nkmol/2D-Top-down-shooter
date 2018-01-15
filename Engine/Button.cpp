c19cf3e7c94442537562a7520b7f9555baabea8f
nkop <nkop1@avans.nl>
2018-01-15 10:25:17 +0100

    refactor (#97)

--- Engine/Button.cpp
+++ Engine/Button.cpp
@@ -3,19 +3,10 @@
 #include "Event.h"
 #include "InputManager.h"
 
-
-Button::Button(const std::string& mediatoken, int x1, int y1, int width, int height) : GameObject(Point(x1, y1), width, height) {
-	x1Pos = x1;
-	y1Pos = y1;
-	setX2andY2(x1, y1, width, height);
-	
-	// TODO Let GameObject handle this
+Button::Button(const std::string& mediatoken, const Point& startPosition, const Point& size) : GameObject(startPosition, size.x, size.y)
+{
 	GameObject::_sprite = AssetManager::Instance().LoadTexture(mediatoken);
 	GameObject::_spriteToken = mediatoken;
-}
-
-Button::Button(const std::string& mediatoken, const Point& startPosition, const Point& size) : Button(mediatoken, startPosition.x, startPosition.y, size.x, size.y)
-{
 }
 
 Button::Button(const std::string& mediatoken, const Point& startPosition, const Point& size, const DelegateFunction&& delegate) : Button(mediatoken, startPosition, size)
@@ -25,26 +16,15 @@ Button::Button(const std::string& mediatoken, const Point& startPosition, const 
 
 Button::Button() {}
 
-void Button::setX2andY2(int x1, int y1, int width, int height)
-{
-	x2Pos = x1 + width;
-	y2Pos = y1 + height;
-}
-
 bool Button::IsClicked(Event ev) const
 {
 	if (!InputManager::Instance().IsMouseClicked(ev)) return false;
 
 	auto& mouse = ev.GetEventValue().button;
-	return mouse.x > x1Pos && mouse.x < x2Pos && mouse.y > y1Pos && mouse.y < y2Pos;
+	return mouse.x > _coordinates.x && mouse.x < _coordinates.x + width && mouse.y > _coordinates.y && mouse.y < _coordinates.y + height;
 }
 
 void Button::Click() const
 {
 	_clickFuncton();
 }
-
-int Button::getX1() { return this->x1Pos; }
-int Button::getY1() { return this->y1Pos; }
-int Button::getY2() { return this->y2Pos; }
-int Button::getX2() { return this->x2Pos; }
--- Engine/headers/Button.h
+++ Engine/headers/Button.h
@@ -8,26 +8,11 @@
 class Button : public GameObject {
 	typedef function<void()> DelegateFunction;
 public:
-	// OLD
-	Button(const std::string& mediatoken, int x1, int y1, int width, int height);
-	// NEW
 	Button(const std::string& mediatoken, const Point& startPosition, const Point& size);
 	Button(const std::string& mediatoken, const Point& startPosition, const Point& size, const DelegateFunction&& del);
 	Button();
-	int getX1();
-	int getY1();
-	int getY2();
-	int getX2();
 	bool IsClicked(Event ev) const;
 	void Click() const;
 private:
 	DelegateFunction _clickFuncton;
-
-	int x1Pos;
-	int x2Pos;
-	int y1Pos;
-	int y2Pos;
-	int width;
-	int height;
-	void setX2andY2(int, int, int, int);
 };
